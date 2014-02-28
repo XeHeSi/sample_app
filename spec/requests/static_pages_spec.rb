5d6ddabf061bcff4ffa955687c2dcde53c2ac3d7
XeHeSi <versus.sokol@gmail.com>
2014-02-28 20:26:34 +0700

    Finish 3 block

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -11,7 +13,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("SampleApp | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -24,20 +26,33 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("SampleApp | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
   describe "About page" do
 
-    it "should have the content 'About Us'" do
+    it "should have the content 'About'" do
       visit '/static_pages/about'
       expect(page).to have_content('About')
     end
 
-    it "should have the title 'About Us'" do
+    it "should have the title 'About'" do
       visit '/static_pages/about'
-      expect(page).to have_title("SampleApp | About")
+      expect(page).to have_title("#{base_title} | About")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
