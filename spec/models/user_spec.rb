7df4a3ea6e0b8e94b2e2bebbe0d495f4b7058308
XeHeSi <versus.sokol@gmail.com>
2014-03-04 17:35:22 +0700

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -74,6 +74,17 @@ describe User do
     it { should_not be_valid }
   end
 
+  # Тест на проверку перевода email в нижний реестр
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
   # ПАРОЛЬ
 
   # Тест на валидацию наличия пароля
